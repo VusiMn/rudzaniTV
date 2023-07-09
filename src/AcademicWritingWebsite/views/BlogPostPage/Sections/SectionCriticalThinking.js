cce19ec3a5f1ee9cd9818b27411abc175b1cc9c0
Vusi Mngomezulu <vusimn@Vusis-MacBook-Air.local>
2023-07-09 19:30:45 +0200

    fix: pitfall highlighted

--- src/AcademicWritingWebsite/views/BlogPostPage/Sections/SectionCriticalThinking.js
+++ src/AcademicWritingWebsite/views/BlogPostPage/Sections/SectionCriticalThinking.js
@@ -2,31 +2,22 @@ import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 // @material-ui/icons
-import TrendingUp from "@material-ui/icons/TrendingUp";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
-import Info from "components/Typography/Info.js";
-import Danger from "components/Typography/Danger.js";
 import Success from "components/Typography/Success.js";
 import Button from "components/CustomButtons/Button.js";
 
 import blogsStyle from "assets/jss/material-kit-pro-react/views/sectionsSections/blogsStyle.js";
 
-import cardBlog4 from "assets/img/examples/card-blog4.jpg";
-import office2 from "assets/img/office2.jpg";
-import blog5 from "assets/img/examples/blog5.jpg";
-import blog6 from "assets/img/examples/blog6.jpg";
-import blog7 from "assets/img/examples/blog7.jpg";
-import blog8 from "assets/img/examples/blog8.jpg";
-import bg5 from "assets/img/bg5.jpg";
 import Quote from "components/Typography/Quote.js";
 import thinkSkillsImage from "assets/img/scientificDiagrams/ThinkSkills1.png";
 import mindMapArgumentBuilding from "assets/img/scientificDiagrams/MindMapArgumentBuilding.png";
 import mindMapArgumentCase from "assets/img/scientificDiagrams/MindMapArgumentCase2.png";
+import Accordion from "components/Accordion/Accordion.js";
 
 const useStyles = makeStyles(blogsStyle);
 
@@ -118,17 +109,7 @@ export default function SectionCriticalThinking({ ...rest }) {
                   </Success>
                 
                 </CardHeader>
-                  <h5 className={classes.description}>
-                    Don{"'"}t be vusi of the truth because we need to restart
-                    the human foundation in truth And I love you like Kanye
-                    loves Kanye I love Rick Owens’ bed design but the back is
-                    too high for the beams and angle of the ceiling I also
-                    wanted to point out that it’s the first album to go number 1
-                    off of streaming...
-                  </h5>
-                  <Button round color="primary">
-                    Read More
-                  </Button>
+            
                 </CardBody>
               </Card>
 
@@ -141,22 +122,39 @@ export default function SectionCriticalThinking({ ...rest }) {
               className={classes.mlAuto + " " + classes.mrAuto}
             >
                
-               <Card plain blog className={classes.card4}>
-                <CardHeader image plain>
-                  <a href="#pablito" onClick={e => e.preventDefault()}>
-                    <img src={thinkSkillsImage} alt="..." />
-                  </a>
-                  <div
-                    className={classes.coloredShadow}
-                    style={{ backgroundImage: `url(${thinkSkillsImage})`, opacity: "1" }}
-                  />
-                </CardHeader>
+    
+                <h2 className={classes.title}>Pitfalls leading to weak and illogical arguments</h2>
+                <Accordion
+                  active={0}
+                  activeColor="rose"
+                  collapses={[
+                    {
+                      title: "1. Premature stance",
+                      content: (
+                        <p>
+                         Deciding on a position or claim before reading widely  to familiarise yourself with other positions similar or different to yours results in a premature stance. Deciding on a position without interrogating other assertions, reasons and evidence leads to a premature stance. 
+                        </p>
+                      )
+                    },
+                    {
+                      title: "2. Engaging superficially with a topic",
+                      content: (
+                        <p>
+                        Superficially engaging with a topic, limited opinions and sources on the topic results in weak arguments. Focus not only on the sources and opinions you agree with, incorporate those you do not agree as the source of a rebuttal to strengthen your arguments. 
+                        </p>
+                      )
+                    },
+                    {
+                      title: "3. To be married to your initial position for better or worse",
+                      content: (
+                        <p>
+                         Be prepared to change your position in light of  valid evidence.  If your position changes, all the other elements of your argument should also change. 
+                        </p>
+                      )
+                    }
+                  ]}
+                />
              
-                <CardBody plain>
-
-
-                </CardBody>
-              </Card>
             </GridItem>
           </GridContainer>
         </div>
